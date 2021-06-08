78a7a7f8a89582b17053f54181aa0ff1ef980f19
yurik <yurik.valder@gmail.com>
2021-06-08 01:30:11 -0400

    fixing bug

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const app = express(); 
 const mongoose = require('mongoose'); 
 const cors = require('cors'); 
-const port = process.env.PORT; 
-
 require('dotenv/config'); 
 
 const port = process.env.PORT || 3000; 
